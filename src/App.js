780e51803c53288d1ef8b4a22c5a7d1b05e14476
LomakinaNataliia <lomakinataliia@gmail.com>
2022-01-26 18:57:47 +0200

    template for the next component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-// import React from "react";
 import "./App.css";
 import Button from "./Button";
 // import Decision from "./Decision";
 
 function App() {
+  // if (button is clicked){
+  //   return (component <Decision />);
+  // }else{}
+
   return (
     <div className="App">
       <h1>hello</h1>
