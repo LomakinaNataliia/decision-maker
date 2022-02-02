e79419add77a99ef0b42cb0aaef23f94a9f8ab09
LomakinaNataliia <lomakinataliia@gmail.com>
2022-02-02 17:33:56 +0200

    cleaned code, added scale property for btn

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import "./App.css";
 import DecisionMaker from "./DecisionMaker";
 
 function App() {
-  // let buttons = document.getElementsByClassName("button"),
-  //   forEach = Array.prototype.forEach;
-  // forEach.call(buttons, function (b) {
-  //   b.addEventListener("click", addElement);
-  // });
-
-  // function addElement() {
-  //   let addDiv = document.createElement("div");
-  //   addDiv.classList.add("pulse");
-  //   this.appendChild(addDiv);
-  //   console.log(addDiv);
-  //   // this.addEventListener("animationend", function (addDiv) {
-  //   //   addDiv.target.remove();
-  //   // });
-  // }
-
   return (
     <div className="App">
       <hr />
@@ -31,7 +15,7 @@ function App() {
           src="https://4fun.one/uploads/posts/t/1584131406.jpeg"
         />
       </div>
-      {/* <button className="button">попробуем</button> */}
+
       <footer>Fasolkin Zad Inc ©</footer>
     </div>
   );
